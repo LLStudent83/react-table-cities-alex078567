80a84dd3d13e4de2852f71928b622893ae4b5d89
Dmitry Prigulnov <prigulnov@motionlogic.ru>
2023-05-22 15:58:33 +0500

    добавил коменты

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import CitiesList from './components/CitiesList/CitiesList';
-import Header from './components/Header/Header';
-import SearchInput from './components/SearchInput/SearchInput';
-import CitiesTable from './components/Table/CitiesTable';
-import { useAppSelector } from './hooks';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import CitiesList from "./components/CitiesList/CitiesList";
+import Header from "./components/Header/Header";
+import SearchInput from "./components/SearchInput/SearchInput";
+import CitiesTable from "./components/Table/CitiesTable";
+import { useAppSelector } from "./hooks";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-	const { showTable } = useAppSelector((store) => store.table);
-	return (
-		<main>
-			<ToastContainer position="top-center" autoClose={2000} />
-			<Header />
-			<SearchInput />
-			<CitiesList />
-			{showTable && <CitiesTable />}
-		</main>
-	);
+  const { showTable } = useAppSelector((store) => store.table);
+  return (
+    <main>
+      <ToastContainer position="top-center" autoClose={2000} />
+      <Header />
+      <SearchInput />
+      <CitiesList />
+      {showTable && <CitiesTable />}
+    </main>
+  );
 }
 
 export default App;
+
+/*
+1. почему в searchCities знаение инпута передается ни через аргумент а через ссылку?
+
+*/
--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,50 +1,50 @@
-import { useEffect, useRef } from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks';
+import { useEffect, useRef } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import {
-	getCities,
-	resetCitiesList,
-	setSearchTerm,
-} from '../../features/search/searchSlice';
+  getCities,
+  resetCitiesList,
+  setSearchTerm,
+} from "../../features/search/searchSlice";
 
 const SearchInput = () => {
-	const { searchTerm } = useAppSelector((store) => store.search);
-	const searchInput = useRef<HTMLInputElement>(null);
-	const dispatch = useAppDispatch();
+  const { searchTerm } = useAppSelector((store) => store.search);
+  const searchInput = useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
 
-	useEffect(() => {
-		searchInput.current?.focus();
-	});
+  useEffect(() => {
+    searchInput.current?.focus();
+  }, []);
 
-	const searchCities = () => {
-		const inputData = searchInput.current?.value;
+  const searchCities = () => {
+    const inputData = searchInput.current?.value;
 
-		dispatch(setSearchTerm(inputData ?? ''));
+    dispatch(setSearchTerm(inputData ?? ""));
 
-		if (inputData) {
-			if (inputData?.length > 2) {
-				dispatch(getCities(inputData));
-			} else {
-				dispatch(resetCitiesList());
-			}
-		}
-	};
+    if (inputData) {
+      if (inputData?.length > 2) {
+        dispatch(getCities(inputData));
+      } else {
+        dispatch(resetCitiesList());
+      }
+    }
+  };
 
-	return (
-		<section className="search-input container">
-			<label className="search-input__label" htmlFor="searchInput">
-				Начните вводить название города
-			</label>
-			<input
-				className="search-input__input"
-				id="searchInput"
-				type="text"
-				placeholder="Например: Самара"
-				value={searchTerm}
-				ref={searchInput}
-				onChange={searchCities}
-			/>
-		</section>
-	);
+  return (
+    <section className="search-input container">
+      <label className="search-input__label" htmlFor="searchInput">
+        Начните вводить название города
+      </label>
+      <input
+        className="search-input__input"
+        id="searchInput"
+        type="text"
+        placeholder="Например: Самара"
+        value={searchTerm}
+        ref={searchInput}
+        onChange={searchCities}
+      />
+    </section>
+  );
 };
 
 export default SearchInput;
